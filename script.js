330aaa8cd15187c3ebdee4ab3d5672cda1355ae9
talesdruzian <tales.druzian@aluno.ifsp.edu.br>
2026-04-03 17:20:00 -0300

    Add trailer input handling in form submission

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ const formulario = document.getElementById('formJogo');
 formulario.addEventListener('submit', function (evento) {
     evento.preventDefault();
     const tituloD = document.getElementById('nome').value;
-    const imagemD = document.getElementById('imagem').value; const novo = {
+    const imagemD = document.getElementById('imagem').value;
+    const trailerD = document.getElementById('trailer').value;
+    const novo = {
         titulo: tituloD,
         imagem: imagemD
     };
