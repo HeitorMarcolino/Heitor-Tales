10ae323af0e6760837093f9f4dde302b50c3f155
talesdruzian <tales.druzian@aluno.ifsp.edu.br>
2026-04-02 23:13:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,3 @@ function alterarFundo(){
 document.body.style.backgroundColor =
 black;
 }
-
-const formulario = document.getElementById('form');
-formulario.addEventListener('submit',function(evento){
-evento.preventDefault();
-const tituloD = document.getElementById('titulo').value;
-const imagemD = document.getElementById('imagem').value;const novo = {
-titulo: tituloD,
-imagem: imagemD
-};});
