fb9d5b7e5588b91c12803cc9b974f6793e456673
talesdruzian <tales.druzian@aluno.ifsp.edu.br>
2026-04-03 17:33:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,15 @@ formulario.addEventListener('submit', function (evento) {
     const tituloD = document.getElementById('nome').value;
     const imagemD = document.getElementById('imagem').value;
     const trailerD = document.getElementById('trailer').value;
+    const sinopseD = document.getElementById('sinopse').value;
+    const dataD = document.getElementById('data').value;
+    const devD = document.getElementById('dev').value;
     const novo = {
         titulo: tituloD,
         imagem: imagemD,
         trailer: trailerD,
+        sinopse: sinopseD,
+        data: dataD,
+        dev: devD,
     };
 });
